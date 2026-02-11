f7d9bfe7a123d51bfd21fb465fde7713c9e09a7d
tylermorganwall <tylermw@gmail.com>
2026-02-11 00:26:16 -0500

    tools

--- tools/replace-block.R
+++ tools/replace-block.R
@@ -13,13 +13,21 @@ read_all = function(path) {
 	rawToChar(raw)
 }
 
+strip_utf8_bom = function(x) {
+	# Remove UTF-8 BOM if present
+	if (nchar(x) > 0 && substr(x, 1, 1) == "\ufeff") {
+		substr(x, 2, nchar(x))
+	} else {
+		x
+	}
+}
+
 normalize_newlines = function(x) {
 	x = gsub("\r\n", "\n", x, fixed = TRUE)
 	x = gsub("\r", "\n", x, fixed = TRUE)
 	x
 }
 
-# Generic normalization: strip trailing whitespace at end-of-line
 normalize_eol_ws = function(x) {
 	gsub("[ \t]+(?=\n)", "", x, perl = TRUE)
 }
@@ -37,9 +45,9 @@ write_all = function(path, txt, use_crlf) {
 	writeBin(charToRaw(txt), con)
 }
 
-txt0 = read_all(fn)
-from0 = read_all(from_fn)
-to0 = read_all(to_fn)
+txt0 = strip_utf8_bom(read_all(fn))
+from0 = strip_utf8_bom(read_all(from_fn))
+to0 = strip_utf8_bom(read_all(to_fn))
 
 use_crlf = detect_crlf(txt0)
 
@@ -49,10 +57,21 @@ to = normalize_newlines(to0)
 
 pos = regexpr(from, txt, fixed = TRUE)[[1]]
 if (pos == -1) {
+	if (nzchar(Sys.getenv("REPLACE_BLOCK_DEBUG", ""))) {
+		message(sprintf(
+			"DEBUG: nchar(txt)=%d nchar(from)=%d",
+			nchar(txt),
+			nchar(from)
+		))
+		message(sprintf("DEBUG: txt_has_crlf=%s", use_crlf))
+		message(sprintf(
+			"DEBUG: from_prefix=%s",
+			encodeString(substr(from, 1, 80), quote = "\"")
+		))
+	}
 	stop(sprintf("Target block not found in '%s' (from_file='%s')", fn, from_fn))
 }
 
 txt_new = sub(from, to, txt, fixed = TRUE)
 write_all(fn, txt_new, use_crlf)
-
 message(sprintf("Replaced block in '%s'", fn))
